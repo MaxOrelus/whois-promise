1dbc2db78df4a3d1e8c4ba1d50616bb4412e18de
Sarah Ashraf <sarahashraf@NYC-sashraf.local>
2020-02-04 16:10:33 -0500

    allow passing options to whois

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ const request = require('./src/utils/request');
 const parse = require('./src/utils/parse');
 const errorHandler = require('./src/utils/errorHandler');
 
-exports.raw = async host => {
+exports.raw = async (host, options) => {
   if (typeof host !== 'string') {
     return new Error('Host value is not a string.');
   }
 
-  return await request(host);
+  return await request(host, options);
 };
 
-exports.json = async host => {
+exports.json = async (host, options) => {
   if (typeof host !== 'string') {
     return new Error('Host value is not a string.');
   }
 
-  const whois = errorHandler(await request(host));
+  const whois = errorHandler(await request(host, options));
 
   if (typeof whois !== 'string') {
     return whois;
